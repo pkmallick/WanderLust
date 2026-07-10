2f693db569981508c0ff0809298a713cf1a39fde
Prakash Kumar Mallick <Prakash@gmail.com>
2026-07-11 01:20:16 +0530

    Fix passport local mongoose import and dependency

--- models/user.js
+++ models/user.js
@@ -1,5 +1,18 @@
+// const mongoose = require("mongoose");
+// const passportLocalMongoose = require("passport-local-mongoose").default;
+
+// const userSchema = new mongoose.Schema({
+//     email: {
+//         type: String,
+//         required: true,
+//     },
+// });
+
+// userSchema.plugin(passportLocalMongoose);
+
+// module.exports = mongoose.model("User", userSchema);
 const mongoose = require("mongoose");
-const passportLocalMongoose = require("passport-local-mongoose").default;
+const passportLocalMongoose = require("passport-local-mongoose");
 
 const userSchema = new mongoose.Schema({
     email: {
